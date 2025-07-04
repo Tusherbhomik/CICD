a1b8ab714665f0aa01e23610152f2afb9b54aa9b
SoumikBhattacharjee31 <soumikbhat330@gmail.com>
2025-07-04 19:22:32 +0600

    minor changes

--- Frontend/src/url.ts
+++ Frontend/src/url.ts
@@ -1,3 +1,1 @@
-// export const API_BASE_URL="http://74.225.140.65:8080"
-export const API_BASE_URL=""
-// export const API_BASE_URL="http://localhost:8080"
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
