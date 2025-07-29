14d1602cd4ed12aca20c6f0cc66fdd872c0cec8c
Tusher Bhomik <2005046@ugrad.cse.buet.ac.bd>
2025-07-29 21:44:34 +0600

    Update url.ts

--- Frontend/src/url.ts
+++ Frontend/src/url.ts
@@ -3,4 +3,4 @@
 //   (typeof import.meta !== "undefined" && import.meta.env?.VITE_API_BASE_URL) ||
 //   "http://localhost:8080";
 
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://74.225.140.65:8080/";
